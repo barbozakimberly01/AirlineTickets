53223f31a1585372df7b1095b4290810658de8d8
ron <ron@192.168.68.147>
2021-11-04 14:15:09 -0400

    Added test method

--- cis3270tp_AirlineReservation/src/edu/gsu/cis3270/tp_AirlineReservation/TestClass.java
+++ cis3270tp_AirlineReservation/src/edu/gsu/cis3270/tp_AirlineReservation/TestClass.java
@@ -2,8 +2,17 @@ package edu.gsu.cis3270.tp_AirlineReservation;
 
 public class TestClass {
 
+	
+	public static void method1() {
+		
+		// This is a simple test method
+		System.out.println("Hello, from method1");
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
