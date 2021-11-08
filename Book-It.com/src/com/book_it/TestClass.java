a91519298ecc4601cc8c6d6b5cd56fd2983bf3db
Ronald Ramsey <mrron26@gmail.com>
2021-11-07 21:00:36 -0500

    Created new project: Book-It.com

--- Book-It.com/src/com/book_it/TestClass.java
+++ Book-It.com/src/com/book_it/TestClass.java
@@ -2,17 +2,8 @@ package com.book_it;
 
 public class TestClass {
 
-	
-	public static void method1() {
-		
-		// This is a simple test method
-		System.out.println("Hello, from method1");
-	}
-	
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 
 	}
 
---
+++ cis3270tp_AirlineReservation/src/com/book_it2/TestClass.java
@@ -0,0 +1,19 @@
+package com.book_it2;
+
+public class TestClass {
+
+	
+	public static void method1() {
+		
+		// This is a simple test method
+		System.out.println("Hello, from method1");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
