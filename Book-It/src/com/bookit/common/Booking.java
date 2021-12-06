86c72dd63a47b99cabad8819cc057dc12fe8595d
kimberlybarboza01 <95250937+kimberlybarboza01@users.noreply.github.com>
2021-12-06 01:47:56 -0500

    Created new booking class

--- Book-It/src/com/bookit/common/Booking.java
+++ Book-It/src/com/bookit/common/Booking.java
@@ -2,17 +2,21 @@ package com.bookit.common;
 
 public class Booking extends Flight {
 	private int bookingID;
+	
 	private String ssn;
 	
 	public int getBookingID() {
 		return bookingID;
 	}
+	
 	public void setBookingID(int bookingID) {
 		this.bookingID = bookingID;
 	}
+	
 	public String getSsn() {
 		return ssn;
 	}
+	
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
 	}
